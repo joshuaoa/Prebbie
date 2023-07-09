235067f7fe97c10188e051643a4392da3fa16980
albertkofi <73664607+albertkofi@users.noreply.github.com>
2023-07-09 12:30:45 +0000

    allowing company_id field to pick default company

--- employee_manager/models/employee_inherit.py
+++ employee_manager/models/employee_inherit.py
@@ -6,3 +6,12 @@ class ModelName(models.Model):
 
     parent_id = fields.Many2one('hr.employee', 'Manager', compute="_compute_parent_id", store=True, readonly=False,
                                 domain="[]")
+
+
+class PartnerInherit(models.Model):
+    _inherit = 'res.partner'
+
+
+     company_id = fields.Many2one('res.company', 'Company', index=True, default=lambda self: self.env.company)
+
+   
