ca1066f382cc78f4834d405429ad1a6b088fd801
albertkofi <albertoappiah8@gmail.com>
2023-07-28 16:08:54 +0000

    Expense sequence

--- expense_sequence/models/expense_inherit.py
+++ expense_sequence/models/expense_inherit.py
@@ -1,7 +1,14 @@
 from odoo import fields, models, api, _
 
-class Discount(models.Model):
+
+class ExpenseSequence(models.Model):
     _inherit = 'hr.expense'
+
+    sequence_number = fields.Char(
+        string="Expense Number",
+        required=True, copy=False,
+        readonly=True,
+        default=lambda self: _('EXP'))
 
     @api.depends('employee_id')
     def _compute_is_ref_editable(self):
@@ -14,7 +21,8 @@ class Discount(models.Model):
             else:
                 expense.is_ref_editable = is_account_manager
 
-
-
-
-
+    @api.model
+    def create(self, vals):
+        if vals.get('sequence_number', _('EXP')) == _('EXP'):
+            vals['sequence_number'] = self.env['ir.sequence'].next_by_code('hr.expense.invoice') or _('EXP')
+        return super(ExpenseSequence, self).create(vals)
